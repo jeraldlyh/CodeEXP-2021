d7bd91b40688cdac4b648ef4b8c1c3457c312a45
charmaineneo <56992719+charmaineneo@users.noreply.github.com>
2021-06-15 16:37:06 +0800

    Add Icons for Bottom Navigation

--- stacks/TabStack.js
+++ stacks/TabStack.js
@@ -4,12 +4,43 @@ import RootProfileStack from "./RootProfileStack";
 import LandingPageStack from "./LandingPageStack";
 import ChatStack from "./ChatStack";
 import BookmarkStack from "./BookmarkStack";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 const TabStack = () => {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, size }) => {
+                let iconName;
+    
+                if (route.name === 'Home') {
+                    iconName = focused
+                    ? 'home'
+                    : 'home-outline';
+                } else if (route.name === 'Bookmark') {
+                    iconName = focused
+                    ? 'star'
+                    : 'star-outline';
+                } else if (route.name === 'Chat') {
+                    iconName = focused
+                    ? 'chatbubble'
+                    : 'chatbubble-outline';
+                } else if (route.name === 'Profile') {
+                    iconName = focused
+                    ? 'person'
+                    : 'person-outline';
+                }
+
+                return <Ionicons name={iconName} size={size} color='red' />;
+                },
+            })}
+            tabBarOptions={{
+                activeTintColor: 'red',
+                inactiveTintColor: 'gray',
+              }}
+        >
             <Tab.Screen name="Home"  component={LandingPageStack} />
             <Tab.Screen name="Bookmark" component={BookmarkStack} />
             <Tab.Screen name="Chat" component={ChatStack} />
