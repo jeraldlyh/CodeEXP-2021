6bc77e9fdb9917b1d10e26f03800a8381614dc80
Jerald <jeraldssw@gmail.com>
2021-06-16 14:13:55 +0800

    Fix file naming error

---
+++ database/actions/User.js
@@ -0,0 +1,62 @@
+import firebase from "../firebaseDB";
+
+// Manual cloud function trigger
+export const createUser = (username) => {
+    firebase.firestore().collection("user")
+        .doc(username)
+        .set({
+            registeredAt: new Date().getTime(),
+            ratings: 0,
+            enableNotification: false,
+            trips: 0,
+            listing: [],
+            reviews: [],
+            bookmarks: []
+        });
+}
+
+export const registerUser = (username, email, password) => {
+    return new Promise((resolve, reject) => {
+        firebase.auth().createUserWithEmailAndPassword(email.trim(), password.trim())
+            .then(userCredential => {
+                userCredential.user.updateProfile({
+                    displayName: username
+                }).then(() => {
+                    createUser(username);
+                    resolve(username);
+                })
+            })
+            .catch(error => {
+                console.log("Error in registerUser");
+                reject(error);
+            });
+    })
+}
+
+export const loginUser = (email, password) => {
+    return new Promise((resolve, reject) => {
+        firebase.auth().signInWithEmailAndPassword(email.trim(), password.trim())
+            .then(userCredential => {
+                resolve(userCredential.user.displayName);
+            })
+            .catch(error => {
+                console.log("Error in loginUser");
+                reject(error);
+            })
+    })
+}
+
+export const getUserProfile = (username) => {
+    return new Promise((resolve, reject) => {
+        firebase.firestore().collection("user")
+            .doc(username)
+            .get()
+            .then(doc => {
+                resolve(doc.data());
+            })
+            .catch(error => {
+                console.log(error);
+                reject(error);
+            })
+    })
+}
