43618ae91bed27fe82565a4b74102c6501fc210e
charmaineneo <56992719+charmaineneo@users.noreply.github.com>
2021-06-16 17:44:37 +0800

    Rating Icon + Register Page

--- stacks/ChatStack.js
+++ stacks/ChatStack.js
@@ -2,6 +2,8 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import ChatScreen from "../components/ChatScreen";
 import RoomScreen from "../components/RoomScreen";
+// import { Button, Icon } from "react-native";
+import { IconButton } from "react-native-paper";
 
 const Chat = createStackNavigator();
 
@@ -9,7 +11,22 @@ const ChatStack = () => {
     return (
         <Chat.Navigator screenOptions={{ headerShown: true, headerStyle: { backgroundColor: "#fa3c4c" }, headerTintColor: "#ffffff" }}>
             <Chat.Screen name="Chat" component={ChatScreen} />
-            <Chat.Screen name="Room" component={RoomScreen} options={({ route }) => ({ title: route.params.anotherUser })}/>
+            {/* <Chat.Screen name="Room" component={RoomScreen} options={{headerRight: () => (
+            <Button
+              onPress={() => alert('This is a button!')}
+              title="Info"
+              color="#fff"
+            />
+          )}}/> */}
+            <Chat.Screen name="Room" component={RoomScreen} options={({ route }) => ({ title: route.params.anotherUser,
+                                                                                        headerRight: () => (
+                                                                                        <IconButton
+                                                                                        icon="thumb-up-outline"
+                                                                                        onPress={() => alert('It works!')}
+                                                                                        color="#ffffff"
+                                                                                        style={{paddingRight: 10}}
+                                                                                        />
+            ) })}/>
         </Chat.Navigator>
     );
 }
